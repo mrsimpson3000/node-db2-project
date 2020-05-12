f9511c411a67cf6a620bb21b04d1ba0cfefb4b43
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-05-12 17:18:30 -0400

    Created second endpoint but unable to test because database is empty. If only I knew how to use seeds...

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -5,4 +5,41 @@ const db = require("../data/dbConnection");
 
 const router = express.Router();
 
+// GETS a list of all accounts from the DB
+router.get("/", (req, res) => {
+  db.select("*")
+    .from("cars")
+    .then((cars) => {
+      res.status(200).json({ data: cars });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: error.message });
+    });
+});
+
+// GETS an account from the DB based on ID
+router.get("/:id", (req, res) => {
+  db("cars")
+    .where({
+      id: req.params.id,
+    })
+    .first()
+    .then((car) => {
+      if (car) {
+        res.status(200).json({ data: car });
+      } else {
+        res
+          .status(404)
+          .json({
+            message: `No cars with the id of ${req.params.id}. Please try another id.`,
+          });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: error.message });
+    });
+});
+
 module.exports = router;
