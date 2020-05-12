d355f9787dd33567aad1a45664bce9fb672bd474
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-05-12 18:49:18 -0400

    UPDATE and DELETE endpoints created. Stretch! I even took the time to type them out and not just copy and paste them as others have done.

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -54,13 +54,63 @@ router.post("/", (req, res) => {
         res.status(500).json({ error: error.message });
       });
   } else {
-    res
-      .status(400)
-      .json({
-        message:
-          "VIN, make, model, and mileage are all required. Please include them all and try again.",
-      });
+    res.status(400).json({
+      message:
+        "VIN, make, model, and mileage are all required. Please include them all and try again.",
+    });
   }
+});
+
+// Makes a change/update to an existing car record
+router.put("/:id", (req, res) => {
+  const changes = req.body;
+  if (isValidCar(changes)) {
+    db("cars")
+      .where({ id: req.params.id })
+      .update(changes)
+      .then((count) => {
+        if (count) {
+          res
+            .status(200)
+            .json({ message: "The car details were updated successfully." });
+        } else {
+          res.status(404).json({
+            message: `There are no cars with the id of ${req.params.id}. Please try another id.`,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message:
+        "VIN, make, model, and mileage are all required. Please include them all and try again.",
+    });
+  }
+});
+
+// Delete a car record based on id
+router.delete("/:id", (req, res) => {
+  db("cars")
+    .where({ id: req.params.id })
+    .del()
+    .then((count) => {
+      if (count) {
+        res.status(200).json({
+          message: `The car with the id of ${req.params.id} was successfully deleted.`,
+        });
+      } else {
+        res.status(404).json({
+          message: `No cars with the id of ${req.params.id} were found. Please choose another id and try again.`,
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: error.message });
+    });
 });
 
 // Checks to see if all required data is included in the request
